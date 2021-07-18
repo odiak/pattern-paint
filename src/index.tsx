a6ac6b71b2fe1b4918239dfbe3b5cc7ce182b7d2
Kaido Iwamoto <h6824.kaido@gmail.com>
2021-07-19 07:25:31 +0900

    Use Vite instead of Webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createGlobalStyle } from 'styled-components'
 import { App } from './App'
 
-const e = document.createElement('div')
-document.body.appendChild(e)
-ReactDOM.render(<App />, e)
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+`
+
+const e = document.getElementById('app')
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  e
+)
